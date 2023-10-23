4d6a67d3b288869b92ada81499fbbfcb414f0eba
Ahmad Salim Maula <101561685+ahmdmaul23@users.noreply.github.com>
2023-10-23 22:05:54 +0700

    Add files via upload
    upload web pln

---
+++ pascabayar/index.php
@@ -0,0 +1,80 @@
+<br><br><br><br><br><br><br><br><br><br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pasca-Bayar</title>
+    <link rel="stylesheet" href="data/css/bootstrap.css" type="text/css">
+</head>
+<body style="background:url('data/images/bg_pln.jpg');">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <div class="panel panel-primary">
+                        <!-- Judul Aplikasi -->
+                        <div class="panel-heading">
+                            <!-- Logo homepage -->
+                            <div style="margin-top: 5px; margin-bottom: 5px;">
+                                <img src="data/images/logo_pln.png" alt="logo" class="logo" width="90px">
+                            </div>
+
+                            <!-- Text Judul -->
+                            <div style="margin-left: 110px; margin-top: -90px; font-size: 200%;">
+                            <strong>HOME PAGE</strong>
+                            </div>
+
+                            <!-- Text Sub judul -->
+                            <div style="margin-left: 110px; font-size: 120%;">
+                            APLIKASI &nbsp; PEMBAYARAN
+                            <br>
+                            LISTRIK &nbsp; PASCA-BAYAR
+                            </div>
+                        </div>
+                        <!-- End Judul Aplikasi -->
+
+                        <!-- Isi -->
+                        <div class="panel-body">
+                            <div class="col-md-12">
+                                <form action="" method="post">
+                                    <div class="form-group">
+                                        <br>
+                                        <table style="width: 100%; font-size: auto; margin-left: center;">
+                                        <th>
+                                            <!-- link di isi pln/index.php -->
+                                            <center>
+                                                <a href="pln/index.php"><label>Menu PLN</label></a>
+                                            </center>
+                                        </th>
+
+                                        <th>
+                                            <!-- link di isi agen/index.php -->
+                                            <center>
+                                                <a href="agen/index.php"><label>Menu AGEN PLN</label></a>
+                                            </center>
+                                        </th>
+                                        </table>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- End Isi -->
+
+                        <!-- Footer -->
+                        <div class="panel-footer">
+                            <center>
+                                <strong>&copy; 2023 - Ahmad Salim Maula</strong>
+                            </center>
+                        </div>
+                        <!-- End Footer -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
